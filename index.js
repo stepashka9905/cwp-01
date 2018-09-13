db0333df5fe69ee79e90b181590742588176ba9c
stepashka9905 <alyonka9905@ya.ru>
2018-09-13 11:22:19 +0300

    Success 4 task

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var name = process.argv[2];
-var surname = process.argv[3];
-console.log(name);
-console.log(surname);
+// var name = process.argv[2];
+// var surname = process.argv[3];
+// console.log(name);
+// console.log(surname);
+var a = [];
+for (let i = 0;i<process.argv.length-2; i++){
+    a[i] = process.argv[i+2];
+    console.log(a[i]);
+}
